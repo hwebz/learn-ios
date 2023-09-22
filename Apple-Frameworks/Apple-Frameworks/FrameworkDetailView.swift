52e7fc71b91ea5c92892df058930e4896c8a3054
Ha Manh Do <hadm.haui@gmail.com>
2023-09-23 01:40:03 +0700

    Framework detail view

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Personal on 23/09/2023.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }.padding()
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+    }
+}
