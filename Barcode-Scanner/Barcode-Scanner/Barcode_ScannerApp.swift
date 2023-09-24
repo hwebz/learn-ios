177cbb197fb5e5bea8c420da176183f782685705
Ha Manh Do <hadm.haui@gmail.com>
2023-09-24 07:29:23 +0700

    Init Barcode Scanner applicaiton

---
+++ Barcode-Scanner/Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by Personal on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
