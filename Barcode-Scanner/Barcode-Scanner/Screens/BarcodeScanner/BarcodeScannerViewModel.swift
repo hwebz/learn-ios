de4c951682ad268ff323983ed1b74cfcc6cd34d6
Ha Manh Do <hadm.haui@gmail.com>
2023-09-24 12:46:48 +0700

    Continue refactoring Barcode application

---
+++ Barcode-Scanner/Barcode-Scanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  BarcodeScannerViewModel.swift
+//  Barcode-Scanner
+//
+//  Created by Personal on 24/09/2023.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    @Published var scannedCode: String = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
