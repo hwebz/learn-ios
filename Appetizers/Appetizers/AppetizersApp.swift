adc4d1acfaa5bf6723c901016c82af478d299b11
Ha Manh Do <hadm.haui@gmail.com>
2023-09-24 13:04:44 +0700

    Init Appetiziers app

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Personal on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
