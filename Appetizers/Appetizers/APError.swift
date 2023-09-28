771cc6e84e4bc37f35a43e1f867d14733649d291
Ha Manh Do <hadm.haui@gmail.com>
2023-09-28 16:58:39 +0700

    Prepare NetworkManager to call API

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Personal on 28/09/2023.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
