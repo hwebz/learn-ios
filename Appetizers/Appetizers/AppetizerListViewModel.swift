df0ee0cff194a13bb087f581c2ba620de344fc40
Ha Manh Do <hadm.haui@gmail.com>
2023-09-28 22:39:36 +0700

    Alert for failed cases

--- Appetizers/Appetizers/AppetizerListViewModel.swift
+++ Appetizers/Appetizers/AppetizerListViewModel.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     // Enable App Transport Security Settings / Allow Arbitrary Loads = true to call API from unknown domain
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    }
                 }
             }
         }
