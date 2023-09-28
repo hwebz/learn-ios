9d826199795b984088a09c27edfc7376a2ff9734
Ha Manh Do <hadm.haui@gmail.com>
2023-09-28 17:11:34 +0700

    get appetizers from API inside list view

--- Appetizers/Appetizers/AppetizerListView.swift
+++ Appetizers/Appetizers/AppetizerListView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State private var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍿 Appetizers")
+        }
+        .onAppear {
+            getAppetizers()
+        }
+    }
+    
+    // Enable App Transport Security Settings / Allow Arbitrary Loads = true to call API from unknown domain
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
