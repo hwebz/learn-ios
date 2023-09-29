ad2fc18e5fb57677f1b41455bd0736db10d19c1c
Ha Manh Do <hadm.haui@gmail.com>
2023-09-29 10:56:57 +0700

    Using AsyncImage

--- Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
+++ Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
@@ -14,9 +14,21 @@ struct AppetizerListCell: View {
         HStack {
 //            Image("sample-appetizer")
 //                .resizable()
-            AppetizerRemoteImage(urlString: appetizer.imageURL)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 120, height: 90)
+//            AppetizerRemoteImage(urlString: appetizer.imageURL)
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 120, height: 90)
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+            } placeholder: {
+                Image("food-placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+            }
                 .cornerRadius(8)
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
