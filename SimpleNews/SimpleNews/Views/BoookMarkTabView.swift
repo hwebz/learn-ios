e436465da4eca30fe47cafa28b29799d329659f4
Ha Manh Do <hadm.haui@gmail.com>
2023-10-08 03:23:17 +0700

    Serach bookmark

--- SimpleNews/SimpleNews/Views/BoookMarkTabView.swift
+++ SimpleNews/SimpleNews/Views/BoookMarkTabView.swift
@@ -10,12 +10,26 @@ import SwiftUI
 struct BoookMarkTabView: View {
     
     @EnvironmentObject var articleBookmarkVM: ArticleBookmarkViewModel
+    @State var searchText: String = ""
     
     var body: some View {
+        let articles = self.articles
+        
         NavigationView {
-            ArticleListView(articles: articleBookmarkVM.bookmarks)
-            .overlay(OverlayView(isEmpty: articleBookmarkVM.bookmarks.isEmpty))
+            ArticleListView(articles: articles)
+            .overlay(OverlayView(isEmpty: articles.isEmpty))
             .navigationTitle("Saved Articles")
+        }
+        .searchable(text: $searchText)
+    }
+    
+    private var articles: [Article] {
+        if (searchText.isEmpty) {
+            return articleBookmarkVM.bookmarks
+        }
+        return articleBookmarkVM.bookmarks.filter {
+            $0.title.lowercased().contains(searchText.lowercased()) ||
+            $0.descriptionText.lowercased().contains(searchText.lowercased())
         }
     }
     
