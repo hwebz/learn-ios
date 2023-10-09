b7ae20c0dbb8bef729239d2df4c0b4bba875dc2c
Ha Manh Do <hadm.haui@gmail.com>
2023-10-10 00:15:01 +0700

    Migrate to share data for multiple platform

---
+++ SimpleNews/SimpleNewsMac/SimpleNewsMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNewsMacApp.swift
+//  SimpleNewsMac
+//
+//  Created by Personal on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNewsMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
