f8cebfbc7fbf6f73d62185ed72e4d8bf220989a6
Ha Manh Do <hadm.haui@gmail.com>
2023-10-10 11:19:53 +0700

    Add Settings for SimpleNews

---
+++ SimpleNews/SimpleNewsMac/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  SimpleNewsMac
+//
+//  Created by Personal on 10/10/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
