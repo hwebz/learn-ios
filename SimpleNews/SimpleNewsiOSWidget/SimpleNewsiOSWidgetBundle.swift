109996973e719ff1c2f19dfb77f1337924de5615
Ha Manh Do <hadm.haui@gmail.com>
2023-10-12 21:01:42 +0700

    Init Widget projects for iOS and Mac

---
+++ SimpleNews/SimpleNewsiOSWidget/SimpleNewsiOSWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNewsiOSWidgetBundle.swift
+//  SimpleNewsiOSWidget
+//
+//  Created by Personal on 12/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SimpleNewsiOSWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SimpleNewsiOSWidget()
+        SimpleNewsiOSWidgetLiveActivity()
+    }
+}
