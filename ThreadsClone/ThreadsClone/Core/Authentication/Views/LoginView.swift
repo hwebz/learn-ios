f4faa6e32f50896c2b1019c3255485cf30de9f33
Ha Manh Do <hadm.haui@gmail.com>
2023-11-21 01:19:29 +0700

    Init Threads Clone project

---
+++ ThreadsClone/ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by Personal on 21/11/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ ThreadsClone/ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Personal on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
