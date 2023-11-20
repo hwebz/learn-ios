91f5acefbe63a4a8a36c51dac44a4d228a746a40
Ha Manh Do <hadm.haui@gmail.com>
2023-11-21 01:52:26 +0700

    Tab Bar Setup

---
+++ ThreadsClone/ThreadsClone/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  ThreadsClone
+//
+//  Created by Personal on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
