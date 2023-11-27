4acdc3a2eaca99f6896ff59bbadf31f3a87da5e0
Ha Manh Do <hadm.haui@gmail.com>
2023-11-28 00:55:04 +0700

    Firebase Setup & Integration (FirebaseAuth, FirebaseStorage, FirebaseFirestore, FirebaseFirestoreSwift

---
+++ ThreadsClone/ThreadsClone/App/ThreadsCloneApp.swift
@@ -0,0 +1,29 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Personal on 21/11/2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+@main
+struct ThreadsCloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ThreadsTabView()
+        }
+    }
+}
