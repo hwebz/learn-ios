a2c94414b5ce04a62f5ec183251fd72e100937f0
Ha Manh Do <hadm.haui@gmail.com>
2023-12-11 17:21:16 +0700

    Fetching Threads for Feed

--- ThreadsClone/ThreadsClone/Extensions/PreviewProvider.swift
+++ ThreadsClone/ThreadsClone/Extensions/PreviewProvider.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import Firebase
 
 extension PreviewProvider {
     static var dev: DeveloperPreview {
@@ -18,4 +19,6 @@ class DeveloperPreview {
     static let shared = DeveloperPreview()
     
     let user = User(id: NSUUID().uuidString, fullname: "Ha Manh Do", email: "hatest@gmail.com", username: "hadm")
+    
+    let thread = Thread(ownerUid: "123", caption: "This is a test thread", timestamp: Timestamp(), likes: 0)
 }
